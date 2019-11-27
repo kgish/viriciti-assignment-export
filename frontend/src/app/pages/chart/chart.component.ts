6347219cb355d2ab042609323325126cde0d931b
Kiffin Gish <kiffin.gish@planet.nl>
2019-11-27 22:10:17 +0100

    Chart x-axis type time.

--- frontend/src/app/pages/chart/chart.component.ts
+++ frontend/src/app/pages/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 
-import { ChartService } from '../../services';
+import { ChartService, IChartData } from '../../services';
 
 
 @Component({
@@ -10,13 +10,30 @@ import { ChartService } from '../../services';
 })
 export class ChartComponent implements OnInit, AfterViewInit {
 
+  data: IChartData[];
+
   constructor(private chartService: ChartService) {
   }
 
   ngOnInit() {
+    this.data = [
+      { x: 1, y: this._randomScalingFactor() },
+      { x: 2, y: this._randomScalingFactor() },
+      { x: 3, y: this._randomScalingFactor() },
+      { x: 4, y: this._randomScalingFactor() },
+      { x: 5, y: this._randomScalingFactor() },
+      { x: 6, y: this._randomScalingFactor() },
+      { x: 9, y: this._randomScalingFactor() },
+    ];
   }
 
   ngAfterViewInit(): void {
-    this.chartService.renderChart('chart');
+    this.chartService.renderChart('chart', this.data);
+  }
+
+  // Private
+
+  _randomScalingFactor() {
+    return Math.round(-100 + 200 * Math.random());
   }
 }
